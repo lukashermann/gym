cc19360d3c5e8142bbe9bfba2cb081b577a6a50a
Lukas Hermann <lukas.hermann@gmx.de>
2017-11-07 14:27:50 +0100

    col pen and done reward

--- gym/envs/mujoco/jaco.py
+++ gym/envs/mujoco/jaco.py
@@ -4,7 +4,7 @@ from gym.envs.mujoco import mujoco_env
 
 class JacoEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
-        self.col_pen = 0#-0.5
+        self.col_pen = -0.3
         utils.EzPickle.__init__(self)
         mujoco_env.MujocoEnv.__init__(self, 'jaco/jaco.xml', 2)
         self.width = 200
@@ -13,12 +13,15 @@ class JacoEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def _step(self, a):
         vec = self.get_body_com("jaco_fingertips")-self.get_body_com("target")
-        reward_dist = - 0.1 * np.linalg.norm(vec)
+        reward_dist = - 0.2 * np.linalg.norm(vec)
         reward_ctrl = - 0.1 * np.square(a).sum()
         reward = reward_dist + reward_ctrl + self.col_pen * self.detect_col()
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
         done = False
+        if np.linalg.norm(vec) < 0.06:
+            reward +=1
+            done = True
 
         #qpos = np.random.uniform(low=-0.1, high=0.1, size=self.model.nq) + self.init_qpos
         #qpos = np.array([0,0.5,-3,0,0,0,0,0])
