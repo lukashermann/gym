94151d70b85fdadb1db55155d904e4109c4d70bd
Lukas Hermann <lukas.hermann@gmx.de>
2017-11-16 16:01:25 +0100

    penalize collision of target and arm

--- gym/envs/mujoco/jaco_push.py
+++ gym/envs/mujoco/jaco_push.py
@@ -4,7 +4,7 @@ from gym.envs.mujoco import mujoco_env
 
 class JacoPushEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
-        self.col_pen = 0#-0.5
+        self.col_pen = -1
         utils.EzPickle.__init__(self)
         mujoco_env.MujocoEnv.__init__(self, 'jaco_push/jaco_push.xml', 2)
         self.width = 500
@@ -17,25 +17,32 @@ class JacoPushEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         reward_target_dist = - 0.2 * np.linalg.norm(vec)
         reward_ctrl = - 0.1 * np.square(a).sum()
         reward_goal_dist = - np.linalg.norm(vec_2)
-        reward = reward_target_dist + reward_ctrl + reward_goal_dist + self.col_pen * self.detect_col()
+        reward = reward_target_dist + reward_ctrl + reward_goal_dist + self.col_pen * self.detect_col_with_target()
 
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
         done = False
-        if np.linalg.norm(vec_2) < 0.02:
+        if np.linalg.norm(vec_2) < 0.02 and (abs(self.model.data.qvel[-4:-2]) < 0.5).all():
             reward +=2
             done = True
+
         #qpos = np.random.uniform(low=-0.1, high=0.1, size=self.model.nq) + self.init_qpos
         #qpos = np.array([0,0.5,-3,0,0,0,0,0])
         #self.set_state(qpos, self.model.data.qvel.flat.copy())
 
         return ob, reward, done, dict(reward_target_dist=reward_target_dist, reward_ctrl=reward_ctrl, reward_goal_dist=reward_goal_dist)
 
-    def detect_col(self):
+    def detect_col_with_target(self):
         if self.data.ncon > 0:
-            return 1
-        else:
-            return 0
+            for coni in range(self.data.ncon):
+                con = self.data.obj.contact[coni]
+                # contact with hand
+                #if con.geom1 == 13 and con.geom2 in [10,11,12]:
+                # contact with arm
+                if con.geom1 == 13 and con.geom2 in [7,8,9]:
+                    return 1
+
+        return 0
 
     def viewer_setup(self):
         # keep camera fixed in scene
@@ -53,7 +60,7 @@ class JacoPushEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         qpos[1] = np.random.uniform(-1,0.5)
         qpos[2] = np.random.uniform(-np.pi,0)
         """
-        qpos[0] = np.random.uniform(-np.pi/2-0.1,-np.pi/2+0.1)
+        qpos[0] = -2#np.random.uniform(-np.pi/2-0.1,-np.pi/2+0.1)
         qpos[1] = np.random.uniform(-0.1,0.1)
         qpos[2] = np.random.uniform(-np.pi/2+0.1,-np.pi/2+0.1)
         qpos[3] = -np.pi/2
